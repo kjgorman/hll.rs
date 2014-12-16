c80aed7d6ff562cdbe3f6ba10b8a2a1acdebd90d
kjgorman <kieran@kjgorman.com>
2014-12-16 23:03:52 +1300

    Add tests for counting after merging

--- tests/addition.rs
+++ tests/addition.rs
@@ -4,6 +4,8 @@
 
 extern crate "basic-hll" as hll;
 
+use std::num::Float;
+
 #[test]
 fn adding_two_empty_hlls_results_in_an_empty_hll () {
     let first  = hll::HLL::ctor(0.26);
@@ -24,4 +26,34 @@ fn adding_together_two_hlls_takes_the_max_in_each_register () {
     let third = first + second;
     assert_eq!(third.registers()[14], 7);
     assert_eq!(third.registers()[6], 5);
+}
+
+#[test]
+fn adding_together_two_hlls_preserves_the_sum_of_their_counts () {
+    let mut first  = hll::HLL::ctor(0.0040625);
+    let mut second = hll::HLL::ctor(0.0040625);
+
+    first.insert("a"); first.insert("b"); first.insert("c");
+    second.insert("d"); second.insert("e"); second.insert("f");
+
+    assert_eq!(first.count().round(), 3.0);
+    assert_eq!(second.count().round(), 3.0);
+
+    let third = first + second;
+    assert_eq!(third.count().round(), 6.0);
+}
+
+#[test]
+fn add_together_two_hlls_doesnt_double_count_duplicate_elements () {
+    let mut first  = hll::HLL::ctor(0.0040625);
+    let mut second = hll::HLL::ctor(0.0040625);
+
+    first.insert("a"); first.insert("b"); first.insert("c");
+    second.insert("a"); second.insert("d"); second.insert("e");
+
+    assert_eq!(first.count().round(), 3.0);
+    assert_eq!(second.count().round(), 3.0);
+
+    let third = first + second;
+    assert_eq!(third.count().round(), 5.0);
 }
